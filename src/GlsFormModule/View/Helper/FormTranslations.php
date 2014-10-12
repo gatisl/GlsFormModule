0162f4304b0e124e241ff54fd64a0c71750088c5
Gatis Latkovskis <gatis.latkovskis@gmail.com>
2014-10-12 21:31:40 +0300

    Added more optional arguments to formTranslations view helper, for more customized rendering

--- src/GlsFormModule/View/Helper/FormTranslations.php
+++ src/GlsFormModule/View/Helper/FormTranslations.php
@@ -11,25 +11,29 @@ class FormTranslations extends BaseAbstractHelper
 {
     private $errorViewHelper;
     
+    private $num = 0;
+    
     public function __construct($errorViewHelper) 
     {
         $this->errorViewHelper = $errorViewHelper;
     }
 
-    public function __invoke(ElementInterface $element)
+    public function __invoke(ElementInterface $element, $renderLabel = true, array $whiteList = array(), array $blackList = array())
     {
         if (! $element instanceof TranslateableElement) {
             throw new \InvalidArgumentException('Element not instance of GlsFormModule\Form\Element\TranslateableElement');
         }
         
-        return $this->render($element);
+        $this->num++;
+        
+        return $this->render($element, $renderLabel, $whiteList, $blackList);
     }
     
-    private function render(TranslateableElement $element)
+    private function render(TranslateableElement $element, $renderLabel, $whiteList, $blackList)
     {
         return 
             $this->renderTabs($element) .
-            $this->renderTabContent($element)
+            $this->renderTabContent($element, $renderLabel, $whiteList, $blackList)
         ;
     }
     
@@ -44,7 +48,7 @@ class FormTranslations extends BaseAbstractHelper
         $html = '<ul class="nav nav-tabs" role="tablist">';
         foreach ($element->getLocales() as $key => $locale) {
             $label = array_key_exists($locale, $element->getLocaleLabels()) ? $element->getLocaleLabels()[$locale] : $locale;
-            $html .= sprintf('<li class="%s"><a href="#%s"  role="tab" data-toggle="tab" >%s</a></li>', $key === 0 ? 'active' : '', $locale, $label);
+            $html .= sprintf('<li class="%s"><a href="#%s"  role="tab" data-toggle="tab" >%s</a></li>', $key === 0 ? 'active' : '', $this->getTabName($locale), $label);
         }
         $html .= '</ul>';
         $html .= '<div class="sp23"></div>';
@@ -52,19 +56,32 @@ class FormTranslations extends BaseAbstractHelper
         return $html;
     }
     
-    protected function renderTabContent(TranslateableElement $translateableElement)
+    protected function renderTabContent(TranslateableElement $translateableElement, $renderLabel, $whiteList, $blackList)
     {
         $html = '<div class="tab-content">';
         
         foreach ($translateableElement->getLocales() as $key => $locale) {
-            $html .= sprintf('<div class="tab-pane %s" id="%s">', $key === 0 ? 'active' : '', $locale);
+            $html .= sprintf('<div class="tab-pane %s" id="%s">', $key === 0 ? 'active' : '', $this->getTabName($locale));
             
             $fieldset = $translateableElement->get($locale);
             /* @var $fieldset FieldsetInterface*/
             foreach ($fieldset->getElements() as $element) {
+                
+                
+                if (! empty($whiteList) && ! $this->inList($whiteList, $fieldset->getName(), $element->getName())) {
+                    continue;
+                }
+                
+                if (! empty($blackList) && $this->inList($blackList, $fieldset->getName(), $element->getName())) {
+                    continue;
+                }
+                
                 $html .= sprintf('<div class="form-group %s">', count($element->getMessages()) > 0 ? 'has-error' : '');
-                $html .= $this->renderHelper('formLabel', $element);
-                $html .= '<div class="sp7"></div>';
+                if ($renderLabel === true) {
+                    $html .= $this->renderHelper('formLabel', $element);
+                    $html .= '<div class="sp7"></div>';
+                }
+                
                 $html .= $this->renderHelper('formElement', $element);
                 $html .= $this->renderHelper($this->errorViewHelper, $element);
                 $html .= '</div>';
@@ -75,5 +92,20 @@ class FormTranslations extends BaseAbstractHelper
         $html .= '</div>';
         
         return $html;
+    }
+    
+    private function getTabName($locale)
+    {
+        return sprintf('locale_tab_%d_%s', $this->num, $locale);
+    }
+    
+    private function inList($list, $fieldsetName, $elemName)
+    {
+        foreach ($list as $item) {
+            if (sprintf('%s[%s]', $fieldsetName, $item) === $elemName) {
+                return true;
+            }
+        }
+        return false;
     }
 }
